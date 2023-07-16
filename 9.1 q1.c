1fcfd983b354a37f7d6b09a9efa7f11371862e39
hetvi nabhoya <hetunabhoya31@gmail.com>
2023-07-16 23:07:05 +0530

    string

---
+++ 9.1 q1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+	int i;
+    char a[100];
+    
+    printf("Enter any name :-  ");
+    scanf("%s",&a);
+    
+    
+    for(i=0;i<=a[i];i++)
+    {
+      if(a[i]>=65 && a[i]<=90)
+       printf("%c",a[i]);
+       else if(a[i]>=97 && a[i]<=122)  
+    
+		printf("%c",a[i]-32);
+	}
+           	  	
+}
---
+++ 9.1 q2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+	int i;	
+	char a[100];
+	
+	printf("Enter any name :-  ");
+	scanf("%s",&a);
+	
+	for(i=0;i<=a[i];i++)
+	{
+		if(a[i]>=65 && a[i]<=90)
+		printf("%c",a[i]+32);
+		else if(a[i]>=97 && a[i]<=122)
+		
+		printf("%c",a[i]);
+	}
+	
+}
